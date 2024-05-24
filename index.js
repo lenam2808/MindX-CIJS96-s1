9760e1c265f601731ab302d4e85b50309e9beb2e
Nam Le <lenam28082000@gmail.com>
2024-05-24 23:57:20 +0700

    them tu bai 8 den bai 13

--- index.js
+++ index.js
@@ -90,36 +90,181 @@ console.log(highestSalaryEmployee)
 // }
 
 //Bai 8
-let highestWorkDays = 0;
-for (let { workingDays, lateDays } of employees) {
-  let workDays = workingDays - lateDays;
-  
-  // console.log(workDays)
-  if (workDays > highestWorkDays) {
-    highestWorkDays = workDays;
-  }
-}
+// const employees = [
+//   { id: 1, name: "John", workingDays: 22, lateDays: 2 },
+//   { id: 2, name: "Jane", workingDays: 20, lateDays: 0 },
+//   { id: 3, name: "Mark", workingDays: 25, lateDays: 1 },
+// ];
 
-let wordhardEmployee = employees.filter((employee) => {
-    let workDays = (employee.workingDays - employee.lateDays)
-    // console.log(workDays)
-    if(workDays == highestWorkDays) {
-      return `id: ${employee.id}, name: ${employee.name}, workingDays: ${employee.workingDays}, lateDays: ${employee.lateDays}`
-    }
-  })
-  console.log(wordhardEmployee)
+// let max = employees[0].workingDays - employees[0].lateDays;
+// let indexMax = 0;
 
-// for (let { id, name, workingDays, lateDays } of employees) {
-//   let workDays = workingDays - lateDays;
-//   if (highestWorkDay == workDays) {
-//     console.log(
-//       `id: ${id}, name: ${name}, workingDays: ${workingDays}, lateDays: ${lateDays}`
-//     );
+// for (let i=0; i<employees.length; i++) {
+//   const element = employees[i];
+//   const worked = element.workingDays - element.lateDays;
+//   if(worked > max) {
+//       max = worked;
+//       indexMax = i;
 //   }
 // }
+// console.log(employees[indexMax])
 
 //Bai 9
-// const groupEmployees = 
+// const employees = [
+//     { id: 1, name: "John", salary: 2000 },
+//     { id: 2, name: "Jane", salary: 2500 },
+//     { id: 3, name: "Mark", salary: 3000 },
+//     { id: 4, name: "John", salary: 2200 },
+// ];
 
+// const result = {};
 
-// groupEmployees();
+// const listName = employees.map(item => item.name)
+
+// listName.forEach((item, idx) => {
+//     if(!result[item]) {
+//         result[item] = [];
+//     }
+//     result[item].push(employees[idx])
+// })
+
+// console.log(result)
+
+//Bai 10
+// const employees = [
+//   { id: 1, name: "John", workingDays: 22, lateDays: 2, salary: 2000 },
+//   { id: 2, name: "Jane", workingDays: 25, lateDays: 0, salary: 2500 },
+//   { id: 3, name: "Mark", workingDays: 20, lateDays: 1, salary: 3000 },
+// ];
+
+// let max = employees[0].workingDays / employees[0].salary;
+// let indexMax = 0;
+
+// for (let i=0; i<employees.length; i++) {
+//     const element = employees[i];
+//     const result = element.workingDays / element.salary;
+//     console.log(result)
+//     if(result > max) {
+//         max = result;
+//         indexMax = i;
+//     }
+// }
+// console.log(employees[indexMax])
+
+//Bai 11
+
+// const employees = [
+//     { id: 1, name: "John", workingDays: 20 },
+//     { id: 2, name: "Jane", workingDays: 22 },
+//     { id: 3, name: "Mark", workingDays: 20 },
+//     { id: 4, name: "Sam", workingDays: 24 },
+//     { id: 5, name: "Lucy", workingDays: 22 },
+// ];
+
+// const result = {};
+
+// employees.forEach(item => {
+//     if(!result[item.workingDays]) {
+//         result[item.workingDays] = 0;
+//     }
+//     result[item.workingDays]++;
+// })
+// console.log(result)
+
+//Bai 12
+// const employees = [
+//   {
+//     id: 1,
+//     name: "John",
+//     email: "john@example.com",
+//     workingDays: 22,
+//     lateDays: 2,
+//     salary: 2000,
+//   },
+//   {
+//     id: 2,
+//     name: "Jane",
+//     email: "jane@example.com",
+//     workingDays: 20,
+//     lateDays: 0,
+//     salary: 2500,
+//   },
+//   {
+//     id: 3,
+//     name: "Mark",
+//     email: "mark@example.com",
+//     workingDays: 25,
+//     lateDays: 1,
+//     salary: 3000,
+//   },
+// ];
+
+// const newArr = employees.map((item) => {
+//   return {
+//     name: item.name,
+//     email: item.email,
+//     workInfo: {
+//       workingDays: item.workingDays,
+//       lateDays: item.lateDays
+//     }
+//   }
+// })
+
+// console.log(newArr)
+
+//Bai13
+let tasks = [
+  {
+    name: "Hoàn thành bài tập JavaScript",
+    description: "Làm xong bài tập về Array và Object",
+    completed: false,
+  },
+  { 
+    name: "Đọc sách", 
+    description: "Đọc xong chương 3", 
+    completed: true 
+  },
+  {
+    name: "Đi mua hàng",
+    description: "Mua thêm thức ăn cho tuần",
+    completed: false,
+  },
+];
+
+const addTask = (name, desc, status) => {
+  const newTask = {
+    name: name,
+    description: desc,
+    completed: status
+  };
+  tasks.push(newTask);
+}
+
+addTask("choi game","aaa",false)
+console.log(tasks)
+
+const checkCompleted = (...args) => {
+  tasks.forEach(item => {
+    args.forEach(arg => {
+      if(item.name === arg) {
+        item.completed = true;
+      }
+    })
+  })
+}
+
+checkCompleted("Đi mua hàng", "Hoàn thành bài tập JavaScript")
+console.log(tasks)
+
+const sortTask = () => {
+  tasks.sort((a,b) => a.name.localeCompare(b.name))
+}
+
+sortTask()
+console.log(tasks)
+
+const reverseTask = () => {
+  tasks.sort((a,b) => b.name.localeCompare(a.name))
+}
+reverseTask();
+console.log(tasks)
